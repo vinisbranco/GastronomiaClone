7ccfa925b3d03bfb154bd7fa0626f6345fe1c04a
Rafael Barni Ritter <rafaritter44@hotmail.com>
2018-11-11 01:08:37 -0200

    rm bin

--- bin/WebContent/app/main.js
+++
@@ -1,8 +0,0 @@
-angular.module('app',['ngRoute']).config(function ($routeProvider, $locationProvider){
-  $locationProvider.html5Mode(true);
-
-  $routeProvider.when('/',{
-    templateUrl: 'public/html/home.html',
-    controller: 'appController'
-  });
-});
