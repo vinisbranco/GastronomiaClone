33abe854c1df65531a80c8ec999138210e7aeb49
vinisbranco <vinicius.branco@acad.pucrs.br>
2018-11-13 16:35:26 -0200

    testes AtributosBO

---
+++ test/testMockitoAtributoBO/AtributoBOTest.java
@@ -0,0 +1,45 @@
+package testMockitoAtributoBO;
+
+import br.com.gastronomia.bo.AtributoBO;
+import br.com.gastronomia.bo.UsuarioBO;
+import br.com.gastronomia.dao.AtributoDAO;
+import br.com.gastronomia.dao.UsuarioDAO;
+import br.com.gastronomia.exception.ValidationException;
+import br.com.gastronomia.model.Atributo;
+import br.com.gastronomia.model.Usuario;
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+public class AtributoBOTest {
+	private AtributoBO atributoBO;
+	private Atributo atributo;
+	private AtributoDAO atributoDAO;
+	
+	@Before
+	public void setup() {
+		atributoDAO = mock(AtributoDAO.class);
+		atributoBO = new AtributoBO();
+		atributoBO.setAtributoDAO(atributoDAO);
+		atributo = new Atributo("TesteAtributo",2);
+	}
+	
+	@Test
+	public void TestCreateAtributo() throws ValidationException {
+		Assert.assertTrue(atributoBO.createAtributo(atributo));
+	}
+	@Test
+	public void TestCreateAtributoFalse() throws ValidationException {
+		try {
+			Atributo atributoFalse = null;
+			atributoBO.createAtributo(atributoFalse);
+			}catch (Exception e) {
+				System.err.print(e);
+				return;
+			}
+			fail();
+	}
+}
